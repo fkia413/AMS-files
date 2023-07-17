36fb7f69f97437f48cfe9592165c0a79b4c3613a
Farah Kia <farah.riahikia@gmail.com>
2023-07-17 12:56:23 +0100

    error with loading local data so trying to sort

---
+++ Menagerie.sql
@@ -0,0 +1,4 @@
+mysql> Source C:\Users\xmika\Downloads\menagerie-db\load_pet_tbl.sql  
+Query OK, 1 row affected (0.00 sec)
+
+ERROR 3948 (42000): Loading local data is disabled; this must be enabled on both the client and server sides
