7c40323aa63cfcfe82379832a9af02467486c3cc
Farah Kia <farah.riahikia@gmail.com>
2023-07-17 12:04:43 +0100

    checking how many cities there are - 4079 jeeesus

--- World.sql
+++ World.sql
@@ -1,1 +1,29 @@
 "C:\Users\xmika\Downloads\world-db\world.sql" 
+
+mysql> USE world;
+Database changed
+mysql> SHOW TABLES;
++-----------------+
+| Tables_in_world |
++-----------------+
+| city            |
+| country         |
+| countrylanguage |
++-----------------+
+3 rows in set (0.00 sec)
+
+mysql> SELECT COUNT(*) FROM city;
++----------+
+| COUNT(*) |
++----------+
+|     4079 |
++----------+
+1 row in set (0.00 sec)
+
+mysql> SELECT COUNT(*) FROM country;
++----------+
+| COUNT(*) |
++----------+
+|      239 |
++----------+
+1 row in set (0.00 sec)
