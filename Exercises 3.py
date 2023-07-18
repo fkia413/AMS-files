89b6a946d0138e61536a78a24e72b2e62cb7360d
Farah Kia <farah.riahikia@gmail.com>
2023-07-18 15:16:08 +0100

    To the guy who invented zero, thanks for nothing.

--- Exercises 3.py
+++ Exercises 3.py
@@ -2,21 +2,21 @@
 
 mark = int(input("What mark did you get, Mark?"))
 
-if mark > 85 
+if mark > 85: 
     print("Distinction")
-elif mark > 65 and mark < 85
+elif mark > 65 and mark < 85:
     print("You passed. Kinda...")
-else
+else:
     print("Did you even study???")
 
 
 # Without elif would be
 
-if mark > 85 
+if mark > 85: 
     print("Distinction")
 
-if mark > 65 and mark < 85
+if mark > 65 and mark < 85:
     print("You passed. Kinda...")
 
-if mark < 65
+if mark < 65:
     print("Did you even study???")
