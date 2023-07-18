51655efb2e976f33420fd7f10aecaf77128e53ee
Farah Kia <farah.riahikia@gmail.com>
2023-07-18 16:31:31 +0100

    Can February March? Noo, but April May

---
+++ Exercises 4.py
@@ -0,0 +1,15 @@
+#Iteratives Exercise
+
+statements = 4
+while statements > -1:
+    name = input("Gimme 5 names of people you really don't like: ")
+    print(name + " is da bomb") #706072
+    statements = statements - 1
+
+
+#Explain for loop: it prints numbers between 10 and 21 that are divisible by 2
+
+# Python Conditional Statements & Loops - 44 Exercises good luck to me
+
+# Exercise 1
+
---
+++ Workspace.py
@@ -0,0 +1,4 @@
+numberlist = [3, 56, 78, 55, 39, 99, 1, 13]
+
+for poopie in numberlist:
+    print(poopie)
