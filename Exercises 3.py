fb6d296f72878324c9a45bf04104750127655af1
Farah Kia <farah.riahikia@gmail.com>
2023-07-18 14:12:26 +0100

    To the guy who invented zero, thanks for nothing.

---
+++ Exercises 3.py
@@ -0,0 +1,22 @@
+# Conditionals Exercise
+
+mark = int(input("What mark did you get, Mark?"))
+
+if mark > 85 
+    print("Distinction")
+elif mark > 65 and mark < 85
+    print("You passed. Kinda...")
+else
+    print("Did you even study???")
+
+
+# Without elif would be
+
+if mark > 85 
+    print("Distinction")
+
+if mark > 65 and mark < 85
+    print("You passed. Kinda...")
+
+if mark < 65
+    print("Did you even study???")
