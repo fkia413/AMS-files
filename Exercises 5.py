966375bcbf1703a92a37c0c19a92b90be86e3f84
Farah Kia <farah.riahikia@gmail.com>
2023-07-19 11:06:28 +0100

    hoi.

--- Exercises 5.py
+++ Exercises 5.py
@@ -21,3 +21,17 @@ elif calculation >= 40:
         letter_grade = "E for Elon Musk-level of smartness. I have faith that you'll build us a spaceship to Mars."
 
 print(finalgrade(name, homework_score, assessment_score, final_exam_score, letter_grade))
+
+# W3 Functions Practice
+
+#Question 1 - Write a Python function to find the maximum of 3 numbers:
+
+def maximum(number_1, number_2, number_3, max):
+       return f"Hoi. i is function. i so smart. i give you maximum of 3 numbers you gave me wow: maximum is {max}!!!"
+
+number_1 = int(input("Gimme a number: "))
+number_2 = int(input("Gimme more number ples: "))
+number_3 = int(input("I WANT MORE NUMBER: "))
+max = max(number_1, number_2, number_3) 
+
+print(maximum(number_1, number_2, number_3, max))
