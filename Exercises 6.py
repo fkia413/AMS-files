116aaeeeab72ec40c852c754a99f4a975306a3f8
Farah Kia <farah.riahikia@gmail.com>
2023-07-19 12:17:28 +0100

    German sausage jokes are just the wurst. jajajaja

---
+++ Exercises 6.py
@@ -0,0 +1,9 @@
+# Modules Exercise
+
+# Create 2 new Python files. In one file, write a function that will generate a number between 1 and 6. 
+# Make this a module called dice.
+#In the second file, use the dice module to simulate throwing 2 dice and print the values you get from the dice.
+
+from Workspace import i_like_to_roll_it_roll_it
+
+print(i_like_to_roll_it_roll_it())
--- Workspace.py
+++ Workspace.py
@@ -1,6 +1,12 @@
-numberlist = [3, 56, 78, 55, 39, 99, 1, 13]
+#numberlist = [3, 56, 78, 55, 39, 99, 1, 13]
 
-for poopie in range(4):
-    print(poopie)
+#for poopie in range(4):
+   # print(poopie)
 
 #len as leon
+
+import random
+
+def i_like_to_roll_it_roll_it():
+    return(random.randint(1,6))
+
