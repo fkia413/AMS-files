3dce3b3ff01a08427c317f3e298b772091972f64
Farah Kia <farah.riahikia@gmail.com>
2023-08-09 16:29:15 +0100

    day 3 yay

--- Flask/Exercise_7/app.py
+++ Flask/Exercise_7/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"
 
